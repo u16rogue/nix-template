87fc59386b4e75ce44d6232fd288a59a9ece2d25
u16rogue <u16rogue@ushort.moe>
2026-03-06 02:19:35 +0800

    personal devshell hook to run auto check instead of directly loading extra args

--- dev/.devshellshook.sh
+++ dev/.devshellshook.sh
@@ -1,21 +1,27 @@
 #!/usr/bin/env bash
 
-USE_SHELL="$(which fish)"
-PROGRAM="$(which zellij)"
-PROGRAM_ARGS=""
-
 BWRAP_ARGS+=( \
-    --hostname "devshell-$HOSTNAME"
-    --ro-bind ~/.config/fish ~/.config/fish \
-    --ro-bind ~/.config/zellij ~/.config/zellij \
-    --setenv SHELL "$USE_SHELL" \
+    --hostname "devshell-$HOSTNAME" \
 )
 
-if [[ -f ".zellij.kdl" ]]; then
-    PROGRAM_ARGS="$PROGRAM_ARGS --layout .zellij.kdl"
+if command -v "fish" &> /dev/null; then
+    BWRAP_ARGS+=(--setenv SHELL "$(which fish)")
+    if [[ -e ~/.config/fish ]]; then
+        BWRAP_ARGS+=(--ro-bind ~/.config/fish ~/.config/fish)
+    fi
 fi
 
-if [[ -n "$PROGRAM_ARGS" ]]; then
-    PROGRAM="$PROGRAM -- $PROGRAM_ARGS"
-fi
+if command -v "zellij" &> /dev/null; then
+    PROGRAM="$(which zellij)"
+    if [[ -e ~/.config/zellij ]]; then
+        BWRAP_ARGS+=(--ro-bind ~/.config/zellij ~/.config/zellij)
+    fi
 
+    if [[ -f "$PWD/.zellij.kdl" ]]; then
+        PROGRAM_ARGS="$PROGRAM_ARGS --layout .zellij.kdl"
+    fi
+
+    if [[ -n "$PROGRAM_ARGS" ]]; then
+        PROGRAM="$PROGRAM -- $PROGRAM_ARGS"
+    fi
+fi
