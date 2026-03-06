9e74941dd1578192cc8a7f2c8a8961c580f31bf6
u16rogue <u16rogue@ushort.moe>
2026-03-06 16:37:35 +0800

    reworked personal custom devshell hook; added personal custom zellij layout; fixed program set to term instead of shell; separated program and args from bwrap in exec of shell hook

--- dev/.devshellshook.sh
+++ dev/.devshellshook.sh
@@ -1,27 +1,16 @@
 #!/usr/bin/env bash
 
-BWRAP_ARGS+=( \
-    --hostname "devshell-$HOSTNAME" \
-)
+BWRAP_ARGS+=(--hostname "devshell-$HOSTNAME")
 
-if command -v "fish" &> /dev/null; then
+if command -v fish &> /dev/null; then
     BWRAP_ARGS+=(--setenv SHELL "$(which fish)")
-    if [[ -e ~/.config/fish ]]; then
-        BWRAP_ARGS+=(--ro-bind ~/.config/fish ~/.config/fish)
-    fi
+    [[ -e ~/.config/fish ]] && BWRAP_ARGS+=(--ro-bind ~/.config/fish ~/.config/fish)
 fi
 
-if command -v "zellij" &> /dev/null; then
+if command -v zellij &> /dev/null; then
     PROGRAM="$(which zellij)"
-    if [[ -e ~/.config/zellij ]]; then
-        BWRAP_ARGS+=(--ro-bind ~/.config/zellij ~/.config/zellij)
-    fi
-
-    if [[ -f "$PWD/.zellij.kdl" ]]; then
-        PROGRAM_ARGS="$PROGRAM_ARGS --layout .zellij.kdl"
-    fi
-
-    if [[ -n "$PROGRAM_ARGS" ]]; then
-        PROGRAM="$PROGRAM -- $PROGRAM_ARGS"
-    fi
+    PROGRAM_ARGS=()
+    [[ -e ~/.config/zellij ]] && BWRAP_ARGS+=(--ro-bind ~/.config/zellij ~/.config/zellij)
+    [[ -f "$PWD/.zellij.kdl" ]] && PROGRAM_ARGS+=(--layout .zellij.kdl)
+    PROGRAM_ARGS+=(attach --create "dflt")
 fi
