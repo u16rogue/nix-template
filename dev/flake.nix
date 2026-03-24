b18dbefce9d5e72a57abe2428a194d70c7e48b0f
u16rogue <u16rogue@ushort.moe>
2026-03-25 00:15:58 +0800

    use `flake-parts`

--- dev/flake.nix
+++ dev/flake.nix
@@ -1,23 +1,22 @@
 {
-    description = "Description";
-
-    outputs = { self, nixpkgs }: let
-        system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-    in {
-        devShells.${system}.default = pkgs.mkShell {
-            packages = [];
-
-            shellHook = /*bash*/ ''
-                export NIX_FRAGMENT="default"
-                if [[ -f "$PWD/.devshellshook.sh" ]]; then
-                    source "$PWD/.devshellshook.sh"
-                fi
-            '';
-        };
-    };
-
     inputs = {
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-parts.url = "github:hercules-ci/flake-parts";
+    };
+
+    outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+        flake.description = "Template Description";
+        imports = [];
+        systems = [ "x86_64-linux" ];
+        perSystem = { pkgs, ... }: {
+            devShells.default = pkgs.mkShell {
+                packages = [];
+                shellHook = /*bash*/ ''
+                    if [[ -f "$PWD/.devshellshook.sh" ]]; then
+                        source "$PWD/.devshellshook.sh"
+                    fi
+                '';
+            };
+        };
     };
 }
