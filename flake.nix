c969b7b3eb6dabe099e73ce1733cab1b2a8eff7b
u16rogue <u16rogue@ushort.moe>
2026-05-21 18:50:28 +0800

    bigger floating panel for zellij; use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,10 +1,31 @@
 {
     description = "Nix flake templates";
 
-    outputs = { self }: {
-        templates = {
-            dev.path = ./dev;
-            dev-cpp.path = ./dev-cpp;
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        flake-parts.url = "github:hercules-ci/flake-parts";
+    };
+
+    outputs = inputs@{ flake-parts, ... }: flake-parts.lib.mkFlake { inherit inputs; } {
+        systems = [ "x86_64-linux" ];
+        imports = [];
+        flake = {
+            description = "Template Description";
+            templates = {
+                dev.path = ./dev;
+                dev-cpp.path = ./dev-cpp;
+            };
+        };
+        perSystem = { pkgs, ... }: {
+            devShells.default = pkgs.mkShellNoCC {
+                packages = [];
+                shellHook = /*bash*/ ''
+                    export NIX_FRAGMENT="default"
+                    if [[ -f "$PWD/.devshellshook.sh" ]]; then
+                        source "$PWD/.devshellshook.sh"
+                    fi
+                '';
+            };
         };
     };
 }
